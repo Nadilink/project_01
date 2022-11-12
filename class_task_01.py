f7c8ac302590b65ade012ebd490097e984c07dd9
Nadilink <nadilink@rambler.ru>
2022-11-12 16:15:25 +0300

    class_task_01.py added

---
+++ class_task_01.py
@@ -0,0 +1,24 @@
+
+# Задача 1
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться
+
+my_favorite_songs = 'Waste a Moment', 'Staying\' Alive', 'A Sorta Fairytale', 'Start Me Up', 'New Salvation'
+x = my_favorite_songs
+print(x[0], x[4], x[1], x[-2])
+
+
+
+
+
+
+
+
+
+
+
+

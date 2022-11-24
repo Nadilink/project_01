9940645d14b0beafee4cd31b82353733e144a7bf
Nadilink <nadilink@rambler.ru>
2022-11-24 12:51:28 +0300

    class_task_06 added

---
+++ class_task_06.py
@@ -0,0 +1,56 @@
+
+# Задача 6
+# Есть словарь песен 
+# Распечатайте общее время звучания трех случайных песен
+# Вывод: Три песни звучат ХХХ минут.
+
+import random
+
+my_favorite_songs = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+time_n = []
+
+for i in my_favorite_songs.items():
+        time_n.append(i[1])
+        random.shuffle(time_n)
+
+print(f'Три песни звучат {round(sum(time_n[0:3]), 1)} минут')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

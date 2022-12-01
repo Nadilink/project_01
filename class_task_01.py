92d7a2d87e5ec6e9fdcc820b8273750a7fc05d20
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-12-01 22:54:26 +0300

    Добавил решение

--- class_task_01.py
+++ class_task_01.py
@@ -11,10 +11,35 @@ my_favorite_songs = 'Waste a Moment', 'Staying\' Alive', 'A Sorta Fairytale', 'S
 x = my_favorite_songs
 print(x[0], x[4], x[1], x[-2])
 
+# Решение с помощью индекции строк
+
+first_song = my_favorite_songs [
+    : my_favorite_songs.find(',')
+]
+
+last_song = my_favorite_songs [
+    my_favorite_songs.rfind('A') :
+]
+
+second_song = my_favorite_songs[
+     my_favorite_songs.find('N') : 
+     my_favorite_songs.find(', S')
+]
+
+previous_song = my_favorite_songs [
+    my_favorite_songs.rfind('O') : my_favorite_songs.rfind(',')
+    ]
+
+print(first_song, last_song, second_song, previous_song)
 
 
+# Второй вариант, мы можем воспользоваться методом разделения строк по символам. split.
+# Полученный в результате список проиндексируем по песням
 
+# Решение с помощью метода split() и индексации списков
+songs = my_favorite_songs.split(', ')
 
+print(songs[0], songs[-1], songs[1], songs[-2])
 
 
 
